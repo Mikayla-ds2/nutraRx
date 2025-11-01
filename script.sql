d4b9721cb1d7463f159cb03941dc3ec82ac7c65f
Mikayla Solomon <mikaylasolomon68@gmail.com>
2025-10-31 20:47:00 -0400

    getting examples, interactions separated

--- script.sql
+++ script.sql
@@ -12,6 +12,20 @@ from '/tmp/drug_to_food_interactions.csv'
 DELIMITER ','
 csv HEADER;
 
+select * from interactions
+
 select count(*) from interactions
 
 select count(DISTINCT(food_interactions)) from interactions
+
+select DISTINCT(food_interactions) from interactions
+
+select distinct(food_interactions), count(food_interactions) from interactions
+group by food_interactions
+order by food_interactions desc;
+
+select * from interactions
+where food_interactions like '%Examples%';
+
+select count(*) from interactions
+where food_interactions like '%Examples%';
