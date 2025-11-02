7c97d850e259d4e10bc785ce25580792278cd79b
Mikayla Solomon <mikaylasolomon68@gmail.com>
2025-11-01 23:21:10 -0400

    i'm starting to get tired but where i want to pick up is creating a dictionary of less common interactions that are basically the same as more common ones and then replacing them to reduce distinct interactions

--- script.sql
+++ script.sql
@@ -22,4 +22,42 @@ select count(distinct(name)) from interactions
 
 
 select count(DISTINCT(interaction)) from interactions
+-- i don't care to verify this so 1568 distinct interactions in this dataset --
 
+select name, drug_id, count(interactions) as interaction_count from interactions
+group by name, drug_id
+order by interaction_count desc;
+
+
+select count(interaction) from interactions
+
+select DISTINCT name, interaction from interactions
+order by interaction asc;
+
+select name, interaction from interactions
+where interaction = 'Take with food'
+group by name, interaction
+order by name asc;
+
+
+select interaction, count(DISTINCT(drug_id)) as drug_count from interactions
+group by interaction
+order by drug_count desc;
+
+select drug_id, name from interactions
+where interaction = 'Avoid alcohol'
+and interaction = 'Avoid chocolate'
+-- bc looking for a drug that has both these rows, need to use a different type of query; see below: --
+
+select drug_id, name from interactions
+where interaction in ('Avoid alcohol', 'Take with or without food')
+group by drug_id, name
+having count(DISTINCT interaction) >= 2;
+
+-- thought this would be pretty interesting --
+select i1.interaction as interaction1, i2.interaction as interaction2, count(DISTINCT i1.drug_id) as drug_count
+from interactions i1
+join interactions i2 on i1.drug_id = i2.drug_id and i1.interaction < i2.interaction
+group by i1.interaction, i2.interaction
+order by drug_count desc
+limit 10;
