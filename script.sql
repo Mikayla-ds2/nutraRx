245ad03fa6b19d3caa0bf05cbb6e2c9fd687caa5
Mikayla Solomon <mikaylasolomon68@gmail.com>
2025-11-01 21:10:11 -0400

    believe we've got interactions split up properly; fixing interaction count column now

--- script.sql
+++ script.sql
@@ -3,19 +3,24 @@ drop table if exists interactions;
 
 -- gonna need to change table; made id for drugs - new one would be for interactions, drug_id would just be int --
 create table if not exists interactions (
-    drug_id SERIAL PRIMARY KEY,
+    interaction_id SERIAL PRIMARY KEY,
     name VARCHAR(300),
-    food_interactions VARCHAR(1500)
+    food_interactions VARCHAR(1500),
+    drug_id int,
+    interaction_count int
 )
 
-/copy interactions(drug_id, name, food_interactions)
+/copy interactions(interaction_id, name, food_interactions, drug_id, interaction_count)
 from '/tmp/drug_to_food_interactions.csv'
 DELIMITER ','
 csv HEADER;
 
 select * from interactions
 
-select count(*) from interactions
+select count(distinct(name)) from interactions
+-- there are 1423 distinct drugs in this dataset --
+
+select sum(interaction_count) from interactions;
 
 select count(DISTINCT(food_interactions)) from interactions
 
